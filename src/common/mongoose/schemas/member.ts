223928cd7a6451c18809b23d9b0efd2c0e8b752c
Darkmift <darkmift@gmail.com>
2024-03-23 19:55:01 +0200

    Add Member schema to mongoose

---
+++ src/common/mongoose/schemas/member.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, HydratedDocument } from 'mongoose';
+
+export type MemberDocument = HydratedDocument<Member>;
+
+@Schema()
+export class Member {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true, unique: true })
+  discordUser: string;
+
+  @Prop({ required: true, type: Object })
+  links: {
+    github: string;
+    linkedIn: string;
+  };
+
+  @Prop()
+  description: string;
+}
+
+export const MemberSchema = SchemaFactory.createForClass(Member);
