522ce4beec47d6d7ed3786db8627e9aaeca83037
Darkmift <darkmift@gmail.com>
2024-03-24 13:37:11 +0200

    Add GithubGqlModule and GithubGqlService

---
+++ src/github-gql/github-gql.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { GithubGqlService } from './github-gql.service';
+import { Client, fetchExchange } from '@urql/core';
+import { RootConfig } from '@/config/env.validation';
+
+@Module({
+  providers: [
+    GithubGqlService,
+    {
+      provide: Client,
+      useFactory: (configService: RootConfig) => {
+        return new Client({
+          url: 'https://api.github.com/graphql',
+          exchanges: [fetchExchange],
+          fetchOptions: () => ({
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${configService.GITHUB_TOKEN}`,
+            },
+          }),
+        });
+      },
+      inject: [RootConfig],
+    },
+  ],
+  imports: [RootConfig],
+})
+export class GithubGqlModule {}
