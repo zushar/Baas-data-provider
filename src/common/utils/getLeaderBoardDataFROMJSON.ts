3ba4bd48d8a6ea81720f5ac2dea341aa5d4c188a
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-05-13 16:48:09 +0300

    chore: Update leaderboard service to use JSON data instead of fetching from GitHub API

---
+++ src/common/utils/getLeaderBoardDataFROMJSON.ts
@@ -0,0 +1,499 @@
+export default () => [
+  {
+    name: 'hilell-elsh',
+    node_id: 'MDQ6VXNlcjg0Nzc5NDgy',
+    projects_names: [
+      {
+        url: 'maakaf/maakaf-website',
+        name: 'maakaf-website',
+      },
+    ],
+    avatar_url: 'https://avatars.githubusercontent.com/u/84779482?v=4',
+    score: 5840,
+    stats: {
+      additions: 47,
+      deletions: 2,
+      commits: 1,
+    },
+  },
+  {
+    name: 'hagarfisher',
+    node_id: 'MDQ6VXNlcjI0NzY1ODgy',
+    projects_names: [
+      {
+        url: 'maakaf/maakaf-website',
+        name: 'maakaf-website',
+      },
+    ],
+    avatar_url: 'https://avatars.githubusercontent.com/u/24765882?v=4',
+    score: 4407,
+    stats: {
+      additions: 34,
+      deletions: 5,
+      commits: 1,
+    },
+  },
+  {
+    name: 'EmilyMatt',
+    node_id: 'MDQ6VXNlcjU1NjMxMDUz',
+    projects_names: [
+      {
+        url: 'maakaf/maakaf-website',
+        name: 'maakaf-website',
+      },
+    ],
+    avatar_url: 'https://avatars.githubusercontent.com/u/55631053?v=4',
+    score: 44577,
+    stats: {
+      additions: 324,
+      deletions: 85,
+      commits: 1,
+    },
+  },
+  {
+    name: 'yuvalk',
+    node_id: 'MDQ6VXNlcjI0MDQ0NTk=',
+    projects_names: [
+      {
+        url: 'maakaf/maakaf-website',
+        name: 'maakaf-website',
+      },
+    ],
+    avatar_url: 'https://avatars.githubusercontent.com/u/2404459?v=4',
+    score: 4914,
+    stats: {
+      additions: 25,
+      deletions: 25,
+      commits: 1,
+    },
+  },
+  {
+    name: 'Etinger-Asaf',
+    node_id: 'U_kgDOBgK3cA',
+    projects_names: [
+      {
+        url: 'maakaf/maakaf-website',
+        name: 'maakaf-website',
+      },
+    ],
+    avatar_url: 'https://avatars.githubusercontent.com/u/100841328?v=4',
+    score: 3496,
+    stats: {
+      additions: 25,
+      deletions: 8,
+      commits: 1,
+    },
+  },
+  {
+    name: 'OshriAsulin',
+    node_id: 'U_kgDOBtCgUw',
+    projects_names: [
+      {
+        url: 'maakaf/maakaf-website',
+        name: 'maakaf-website',
+      },
+    ],
+    avatar_url: 'https://avatars.githubusercontent.com/u/114335827?v=4',
+    score: 3010,
+    stats: {
+      additions: 27,
+      deletions: 2,
+      commits: 1,
+    },
+  },
+  {
+    name: 'TomRytt',
+    node_id: 'MDQ6VXNlcjg2OTIxNDYx',
+    projects_names: [
+      {
+        url: 'maakaf/maakaf-website',
+        name: 'maakaf-website',
+      },
+    ],
+    avatar_url: 'https://avatars.githubusercontent.com/u/86921461?v=4',
+    score: 110,
+    stats: {
+      additions: 2,
+      deletions: 2,
+      commits: 1,
+    },
+  },
+  {
+    name: 'eyallampel1',
+    node_id: 'MDQ6VXNlcjE3MTY1MjUy',
+    projects_names: [
+      {
+        url: 'maakaf/maakaf-website',
+        name: 'maakaf-website',
+      },
+    ],
+    avatar_url: 'https://avatars.githubusercontent.com/u/17165252?v=4',
+    score: 3861,
+    stats: {
+      additions: 38,
+      deletions: 1,
+      commits: 1,
+    },
+  },
+  {
+    name: '8hahar',
+    node_id: 'U_kgDOCGUvtA',
+    projects_names: [
+      {
+        url: 'maakaf/maakaf-website',
+        name: 'maakaf-website',
+      },
+    ],
+    avatar_url: 'https://avatars.githubusercontent.com/u/140849076?v=4',
+    score: 374,
+    stats: {
+      additions: 2,
+      deletions: 2,
+      commits: 1,
+    },
+  },
+  {
+    name: 'aek-themis',
+    node_id: 'U_kgDOCEJX-A',
+    projects_names: [
+      {
+        url: 'maakaf/maakaf-website',
+        name: 'maakaf-website',
+      },
+    ],
+    avatar_url: 'https://avatars.githubusercontent.com/u/138565624?v=4',
+    score: 364005,
+    stats: {
+      additions: 3073,
+      deletions: 52,
+      commits: 2,
+    },
+  },
+  {
+    name: 'OliaKr',
+    node_id: 'U_kgDOBxVKIg',
+    projects_names: [
+      {
+        url: 'maakaf/maakaf-website',
+        name: 'maakaf-website',
+      },
+    ],
+    avatar_url: 'https://avatars.githubusercontent.com/u/118835746?v=4',
+    score: 3262,
+    stats: {
+      additions: 104,
+      deletions: 76,
+      commits: 2,
+    },
+  },
+  {
+    name: 'anaalamed',
+    node_id: 'MDQ6VXNlcjc1NTI1OTY0',
+    projects_names: [
+      {
+        url: 'maakaf/maakaf-website',
+        name: 'maakaf-website',
+      },
+    ],
+    avatar_url: 'https://avatars.githubusercontent.com/u/75525964?v=4',
+    score: 3730,
+    stats: {
+      additions: 99,
+      deletions: 37,
+      commits: 2,
+    },
+  },
+  {
+    name: 'YairSadan',
+    node_id: 'U_kgDOB0_aVg',
+    projects_names: [
+      {
+        url: 'maakaf/maakaf-website',
+        name: 'maakaf-website',
+      },
+    ],
+    avatar_url: 'https://avatars.githubusercontent.com/u/122673750?v=4',
+    score: 340,
+    stats: {
+      additions: 8,
+      deletions: 4,
+      commits: 2,
+    },
+  },
+  {
+    name: 'itaim18',
+    node_id: 'MDQ6VXNlcjM3NzcyNzQy',
+    projects_names: [
+      {
+        url: 'maakaf/maakaf-website',
+        name: 'maakaf-website',
+      },
+    ],
+    avatar_url: 'https://avatars.githubusercontent.com/u/37772742?v=4',
+    score: 32865,
+    stats: {
+      additions: 202,
+      deletions: 132,
+      commits: 2,
+    },
+  },
+  {
+    name: 'ZivFisher',
+    node_id: 'MDQ6VXNlcjczMDM3OTk5',
+    projects_names: [
+      {
+        url: 'maakaf/maakaf-website',
+        name: 'maakaf-website',
+      },
+    ],
+    avatar_url: 'https://avatars.githubusercontent.com/u/73037999?v=4',
+    score: 2900,
+    stats: {
+      additions: 32,
+      deletions: 23,
+      commits: 3,
+    },
+  },
+  {
+    name: 'ronnahmias',
+    node_id: 'MDQ6VXNlcjUyMjM3NjE3',
+    projects_names: [
+      {
+        url: 'maakaf/maakaf-website',
+        name: 'maakaf-website',
+      },
+    ],
+    avatar_url: 'https://avatars.githubusercontent.com/u/52237617?v=4',
+    score: 702,
+    stats: {
+      additions: 11,
+      deletions: 5,
+      commits: 4,
+    },
+  },
+  {
+    name: 'roi1410',
+    node_id: 'MDQ6VXNlcjc5OTkwNjg0',
+    projects_names: [
+      {
+        url: 'maakaf/maakaf-website',
+        name: 'maakaf-website',
+      },
+    ],
+    avatar_url: 'https://avatars.githubusercontent.com/u/79990684?v=4',
+    score: 1634,
+    stats: {
+      additions: 107,
+      deletions: 34,
+      commits: 4,
+    },
+  },
+  {
+    name: 'UrielOfir',
+    node_id: 'MDQ6VXNlcjMwMTQ0ODc4',
+    projects_names: [
+      {
+        url: 'maakaf/maakaf-website',
+        name: 'maakaf-website',
+      },
+    ],
+    avatar_url: 'https://avatars.githubusercontent.com/u/30144878?v=4',
+    score: 3934,
+    stats: {
+      additions: 29,
+      deletions: 30,
+      commits: 5,
+    },
+  },
+  {
+    name: 'oshri1997',
+    node_id: 'MDQ6VXNlcjY5NzcwMTEw',
+    projects_names: [
+      {
+        url: 'maakaf/maakaf-website',
+        name: 'maakaf-website',
+      },
+    ],
+    avatar_url: 'https://avatars.githubusercontent.com/u/69770110?v=4',
+    score: 23367,
+    stats: {
+      additions: 189,
+      deletions: 23,
+      commits: 5,
+    },
+  },
+  {
+    name: 'adirkandel',
+    node_id: 'MDQ6VXNlcjEyNTEyOTA5',
+    projects_names: [
+      {
+        url: 'maakaf/maakaf-website',
+        name: 'maakaf-website',
+      },
+    ],
+    avatar_url: 'https://avatars.githubusercontent.com/u/12512909?v=4',
+    score: 93560,
+    stats: {
+      additions: 800,
+      deletions: 51,
+      commits: 7,
+    },
+  },
+  {
+    name: 'DafnaPundak',
+    node_id: 'MDQ6VXNlcjU3MDY2MTE4',
+    projects_names: [
+      {
+        url: 'maakaf/maakaf-website',
+        name: 'maakaf-website',
+      },
+    ],
+    avatar_url: 'https://avatars.githubusercontent.com/u/57066118?v=4',
+    score: 3585,
+    stats: {
+      additions: 122,
+      deletions: 30,
+      commits: 8,
+    },
+  },
+  {
+    name: 'noams24',
+    node_id: 'MDQ6VXNlcjgwODQ5OTk5',
+    projects_names: [
+      {
+        url: 'maakaf/maakaf-website',
+        name: 'maakaf-website',
+      },
+    ],
+    avatar_url: 'https://avatars.githubusercontent.com/u/80849999?v=4',
+    score: 56065,
+    stats: {
+      additions: 1255,
+      deletions: 746,
+      commits: 11,
+    },
+  },
+  {
+    name: 'rotembarsela',
+    node_id: 'MDQ6VXNlcjQ0OTk4NTE0',
+    projects_names: [
+      {
+        url: 'maakaf/maakaf-website',
+        name: 'maakaf-website',
+      },
+    ],
+    avatar_url: 'https://avatars.githubusercontent.com/u/44998514?v=4',
+    score: 234880,
+    stats: {
+      additions: 1784,
+      deletions: 476,
+      commits: 18,
+    },
+  },
+  {
+    name: 'Drorka',
+    node_id: 'U_kgDOBvee4A',
+    projects_names: [
+      {
+        url: 'maakaf/maakaf-website',
+        name: 'maakaf-website',
+      },
+    ],
+    avatar_url: 'https://avatars.githubusercontent.com/u/116891360?v=4',
+    score: 96302,
+    stats: {
+      additions: 758,
+      deletions: 229,
+      commits: 21,
+    },
+  },
+  {
+    name: 'or-schneider',
+    node_id: 'MDQ6VXNlcjkwMDkwMjYw',
+    projects_names: [
+      {
+        url: 'maakaf/maakaf-website',
+        name: 'maakaf-website',
+      },
+    ],
+    avatar_url: 'https://avatars.githubusercontent.com/u/90090260?v=4',
+    score: 94521,
+    stats: {
+      additions: 641,
+      deletions: 302,
+      commits: 26,
+    },
+  },
+  {
+    name: 'develad',
+    node_id: 'MDQ6VXNlcjcwNTcyNjY4',
+    projects_names: [
+      {
+        url: 'maakaf/maakaf-website',
+        name: 'maakaf-website',
+      },
+    ],
+    avatar_url: 'https://avatars.githubusercontent.com/u/70572668?v=4',
+    score: 335959,
+    stats: {
+      additions: 2494,
+      deletions: 1382,
+      commits: 56,
+    },
+  },
+  {
+    name: 'RoyTennenbaum',
+    node_id: 'U_kgDOBzbQmw',
+    projects_names: [
+      {
+        url: 'maakaf/maakaf-website',
+        name: 'maakaf-website',
+      },
+    ],
+    avatar_url: 'https://avatars.githubusercontent.com/u/121032859?v=4',
+    score: 1665977,
+    stats: {
+      additions: 10336,
+      deletions: 4361,
+      commits: 59,
+    },
+  },
+  {
+    name: 'Darkmift',
+    node_id: 'MDQ6VXNlcjEyNDMzNDQx',
+    projects_names: [
+      {
+        url: 'maakaf/maakaf-website',
+        name: 'maakaf-website',
+      },
+      {
+        url: 'maakaf/Baas-data-provider',
+        name: 'Baas-data-provider',
+      },
+    ],
+    avatar_url: 'https://avatars.githubusercontent.com/u/12433441?v=4',
+    score: 881850,
+    stats: {
+      additions: 16718,
+      deletions: 2933,
+      commits: 123,
+    },
+  },
+  {
+    name: 'Tamir198',
+    node_id: 'MDQ6VXNlcjM0NzA3NjY5',
+    projects_names: [
+      {
+        url: 'maakaf/maakaf-website',
+        name: 'maakaf-website',
+      },
+    ],
+    avatar_url: 'https://avatars.githubusercontent.com/u/34707669?v=4',
+    score: 1039501,
+    stats: {
+      additions: 26419,
+      deletions: 24600,
+      commits: 128,
+    },
+  },
+];
--- src/leaderboard/leaderboard.service.ts
+++ src/leaderboard/leaderboard.service.ts
@@ -1,4 +1,4 @@
-import getLeaderboardDataFromGithub from '@/common/utils/getLeaderBoardDataFromGithub';
+import getLeaderBoardDataFROMJSON from '@/common/utils/getLeaderBoardDataFROMJSON';
 import { Injectable, OnModuleInit } from '@nestjs/common';
 
 @Injectable()
@@ -6,14 +6,15 @@ export class LeaderboardService implements OnModuleInit {
   onModuleInit() {
     // Perform initialization tasks here
     console.log('MyService has been initialized');
-    // You can also perform asynchronous operations here
+    // Cron job to fetch and save leaderboard data into the database
   }
 
   async getLeaderboardData() {
-    console.log('getLeaderboardData');
-
-    return (await getLeaderboardDataFromGithub()).data.map(([, data]) => {
-      return data;
-    });
+    // The code below is commented out because should be use when the cron job is implemented
+    // return (await getLeaderboardDataFromGithub()).data.map(([, data]) => {
+    //   return data;
+    // });
+    // For now, we will return a static data as json
+    return getLeaderBoardDataFROMJSON();
   }
 }
