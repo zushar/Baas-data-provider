f420dde6e0bf371e79d7d14ec905089ab6fed626
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-05-14 21:15:13 +0300

    Refactor leaderboard service to use async/await for fetching JSON data

--- src/leaderboard/leaderboard.service.ts
+++ src/leaderboard/leaderboard.service.ts
@@ -1,3 +1,4 @@
+import { AnalyticsDto } from '@/common/dto/leaderboard';
 import getLeaderBoardDataFROMJSON from '@/common/utils/getLeaderBoardDataFROMJSON';
 import { LeaderboardTypeAnalytics } from '@/types/leaderboard';
 import { Injectable, OnModuleInit } from '@nestjs/common';
@@ -10,7 +11,7 @@ export class LeaderboardService implements OnModuleInit {
     // TODO: dto for controller
   }
 
-  async getLeaderboardData() {
+  async getLeaderboardData(): Promise<AnalyticsDto> {
     const since = '2024-01-05T00:00:00Z';
     const until = '2024-04-12T00:00:00Z';
     return {
