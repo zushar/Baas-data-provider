deb83561f474349e058fcce66e3ca1e672d4e1f3
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-05-14 20:26:43 +0300

    Refactor leaderboard service to use JSON data instead of GitHub API

--- src/leaderboard/leaderboard.service.spec.ts
+++ src/leaderboard/leaderboard.service.spec.ts
@@ -1,9 +1,10 @@
-import getLeaderBoardDataFROMJSON from '@/common/utils/getLeaderBoardDataFROMJSON';
 import { LeaderboardAnalyticsSchema } from '@/types/leaderboard';
+import { LeaderboardService } from './leaderboard.service';
 
 describe('GithubGqlService', () => {
+  const service = new LeaderboardService();
   it('Should parse successfully the data from the JSON file', () => {
-    const data = getLeaderBoardDataFROMJSON();
+    const data = service.getLeaderboardData();
     const parsedData = LeaderboardAnalyticsSchema.safeParse(data);
     expect(parsedData.error).toBe(undefined);
   });
