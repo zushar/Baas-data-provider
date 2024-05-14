631e93c4ac144cc38fa42015535d5604134fb9f2
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-05-14 20:27:26 +0300

    Refactor leaderboard service to use async/await for fetching JSON data

--- src/leaderboard/leaderboard.service.spec.ts
+++ src/leaderboard/leaderboard.service.spec.ts
@@ -3,8 +3,8 @@ import { LeaderboardService } from './leaderboard.service';
 
 describe('GithubGqlService', () => {
   const service = new LeaderboardService();
-  it('Should parse successfully the data from the JSON file', () => {
-    const data = service.getLeaderboardData();
+  it('Should parse successfully the data from the JSON file', async () => {
+    const data = await service.getLeaderboardData();
     const parsedData = LeaderboardAnalyticsSchema.safeParse(data);
     expect(parsedData.error).toBe(undefined);
   });
