58c326e23435121fb7655037e7ab6559a0a0891b
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-05-14 21:03:03 +0300

    Refactor leaderboard service to use async/await for fetching JSON data

--- src/leaderboard/leaderboard.controller.spec.ts
+++ src/leaderboard/leaderboard.controller.spec.ts
@@ -1,3 +1,13 @@
-describe('MembersController', () => {
-  it('should be defined', () => {});
+import { LeaderboardAnalyticsSchema } from '@/types/leaderboard';
+import { LeaderboardController } from './leaderboard.controller';
+import { LeaderboardService } from './leaderboard.service';
+
+describe('Leaderboard controller', () => {
+  const service = new LeaderboardService();
+  const controller = new LeaderboardController(service);
+  it('error should be undefined', async () => {
+    const data = controller.getMostRecentDataPaginated();
+    const parsed = LeaderboardAnalyticsSchema.safeParse(data);
+    expect(parsed.error).toBe(undefined);
+  });
 });
