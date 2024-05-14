d7e732424b0eadbb7239ae5a6b3d8dbabe454322
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-05-14 19:44:57 +0300

    change comments

--- src/leaderboard/leaderboard.service.ts
+++ src/leaderboard/leaderboard.service.ts
@@ -4,20 +4,12 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 @Injectable()
 export class LeaderboardService implements OnModuleInit {
   onModuleInit() {
-    // Perform initialization tasks here
-    console.log('MyService has been initialized');
-    // Cron job to fetch and save leaderboard data into the database
     // TODO: add testing for service and controller
     // TODO: move types
     // TODO: dto for controller
   }
 
   async getLeaderboardData() {
-    // The code below is commented out because should be use when the cron job is implemented
-    // return (await getLeaderboardDataFromGithub()).data.map(([, data]) => {
-    //   return data;
-    // });
-    // For now, we will return a static data as json
     return getLeaderBoardDataFROMJSON();
   }
 }
