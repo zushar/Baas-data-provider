d11d2634481a881495fe66cb117d9035b58a89c5
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-05-18 00:20:19 +0300

    Update ProjectV2 schema and projects module

--- src/common/mongoose/schemas/projectV2.ts
+++ src/common/mongoose/schemas/projectV2.ts
@@ -49,8 +49,8 @@ export class ProjectV2 {
           avatarUrl: string | null;
         }[]
       | null;
-    createdAt: string | null | Date;
-    updatedAt: string | null | Date;
+    createdAt: Date | null;
+    updatedAt: Date | null;
   };
 
   @Prop({
