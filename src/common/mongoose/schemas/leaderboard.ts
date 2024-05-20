ceb99e7f1e3edbd35ae8db5fb6d5a27a0ed06d00
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-05-20 17:53:57 +0300

    Refactor leaderboard schema merge conflicts.

--- src/common/mongoose/schemas/leaderboard.ts
+++ src/common/mongoose/schemas/leaderboard.ts
@@ -1,9 +1,31 @@
-import { Schema, SchemaFactory } from '@nestjs/mongoose';
-import { type HydratedDocument } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
 
-export type LeaderboardDocument = HydratedDocument<Leaderboard>;
+export type LeaderboardDocument = Leaderboard & Document;
 
 @Schema()
-export class Leaderboard {}
+export class Leaderboard {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  node_id: string;
+
+  @Prop({ type: [{ url: String, name: String }], required: true })
+  projects_names: { url: string; name: string }[];
+
+  @Prop({ required: true })
+  avatar_url: string;
+
+  @Prop({ required: true })
+  score: number;
+
+  @Prop({ type: Object, required: true })
+  stats: {
+    additions: number;
+    deletions: number;
+    commits: number;
+  };
+}
 
 export const LeaderboardSchema = SchemaFactory.createForClass(Leaderboard);
